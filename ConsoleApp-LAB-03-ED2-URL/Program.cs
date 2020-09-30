af9b808aa1177b6967e37084a0f47e1dfe956977
jamg1906 <jamg1906@gmail.com>
2020-09-29 23:37:34 -0600

    Project initialized

---
+++ ConsoleApp-LAB-03-ED2-URL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp_LAB_03_ED2_URL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
