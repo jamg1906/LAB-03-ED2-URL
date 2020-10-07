d92bc43d44d77d56bc3ba7690b4a7e4ed5f1aec7
jamg1906 <jamg1906@gmail.com>
2020-10-06 23:10:15 -0600

    API: HuffmanController + file post method implemented

---
+++ LAB-03-ED2-URL/Controllers/HuffmanController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LAB_03_ED2_URL.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class HuffmanController : ControllerBase
+    {
+        [HttpPost("compress/{name}")]
+        public async Task<IActionResult> OnPostUploadAsync([FromForm] IFormFile file, [FromRoute] string name)
+        {
+            try
+            {
+                string CompressedName = name;
+                var filePath = Path.GetTempFileName();
+                if (file != null)
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                }
+                else { return Ok("No file was posted"); }
+
+                return Ok("Success");
+            }
+            catch
+            {
+                return BadRequest();
+            }
+
+        }
+
+    }
+}
