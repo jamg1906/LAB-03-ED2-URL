2a0bd5530c62c9a9d515b98dc1300d393f353b88
jamg1906 <jamg1906@gmail.com>
2020-10-07 15:17:32 -0600

    API: Compressions model implemented

---
+++ LAB-03-ED2-URL/Models/Compression.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LAB_03_ED2_URL.Models
+{
+    public class Compression
+    {
+        public string originalName { get; set; }
+        public string compressedFilePath { get; set; }
+        public double compressionRatio { get; set; }
+        public double compressionFactor { get; set; }
+        public double reductionPercentage { get; set; }
+    }
+}
