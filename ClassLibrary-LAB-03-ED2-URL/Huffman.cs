2121820bda537a3f64d867cc8cc0d15ff3310b2c
VelizAndres <dandresveliz@gmail.com>
2020-10-08 20:48:27 -0600

    Modified Compress

--- ClassLibrary-LAB-03-ED2-URL/Huffman.cs
+++ ClassLibrary-LAB-03-ED2-URL/Huffman.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Globalization;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
@@ -9,106 +10,107 @@ namespace ClassLibrary_LAB_03_ED2_URL
 {
     public class Huffman
     {
-       // List<Registro> Tabla;
         Heap<Registro> Cola;
-        Dictionary<string, Registro> Tabla;
-        int Cantidad_nodo;
+        Dictionary<byte, Registro> Tabla;
+        Dictionary<string, Registro> Tabla_Decompres;
+
         int Tam_Original;
         bool Compres;
         public string Resultado_Obtenido()
         {
             string Resul="";
-     /*       foreach(Registro Item in Tabla)
-            {
-                Resul += Item.simbolo;
-                Resul += "|" + Item.Cant_Aparicion + "|" + Convert.ToString(Item.probabilidad) +"|"+ Item.prefijo + Environment.NewLine;
-           }
-     */
-
-        var Contenido=      Tabla.Values;
+             var Contenido=      Tabla.Values;
             foreach (Registro Item in Contenido)
             {
-                //Resul += Item.simbolo;
-                //Resul += "|" + Item.Cant_Aparicion + "|" + Convert.ToString(Item.probabilidad) + "|" + Item.prefijo + Environment.NewLine;
-                Resul += String.Format("{0,5}|{1,20}|{2,30}|{3,35}|" + Environment.NewLine, Item.simbolo, Item.Cant_Aparicion, Convert.ToString(Item.probabilidad), Item.prefijo);
+                     Resul += String.Format("{0,5}|{1,5}|{2,20}|{3,30}|{4,35}|" + Environment.NewLine, Convert.ToChar(Item.simbolo), Item.simbolo, Item.Cant_Aparicion, Convert.ToString(Item.probabilidad), Item.prefijo);
             }
             return Resul;
         }
       
         
-        public string Compresion(string Data)
+        public byte[] Compresion(byte[] Data)
         {
             Compres = true;
             Tam_Original = Data.Length;
-            Tabla = new Dictionary<string, Registro>();
-            string Result = Send_MetaData(CrearRegistros(Data));
+            Tabla = new Dictionary<byte, Registro>();
+            CrearRegistros(Data);
+            byte[] Meta_Data = Send_MetaData();
+            int Tam_Data = Meta_Data.Length;
             Crear_Arbol();
-            Result += Convert_Ascii(Binario_Convert(Data));
-            return Result;
-            //string Titular= String.Format("{0,300}|{1,300}|{2,30}|{3,35}|" + Environment.NewLine, Result, Data, Tam_Original, Result.Length);
-            //return Titular;
+            Agregar_Prefijos(Cola.raiz.Valor);
+            byte[] Result = Convert_Ascii(Binario_Convert(Data));
+            Array.Resize(ref Meta_Data, Meta_Data.Length + Result.Length);
+            Result.CopyTo(Meta_Data, Tam_Data);
+            return Meta_Data;
         }
         
 
-        public string Send_MetaData(List<Registro> Data)
+        public byte[] Send_MetaData()
         {
-            string Resul="";
             int tam = 256;
             int Cant_BFrec = 1;
             int Aux = 0;
+            var Data = Tabla.Values;
             foreach (Registro Item in Data)
             {
-                if (tam<Item.Cant_Aparicion)
-                {
-                    tam += 256;
-                    Cant_BFrec++;
-                }
+                    while (tam < Item.Cant_Aparicion)
+                    {
+                        tam += 256;
+                        Cant_BFrec++;
+                    }
             }
-            Resul += Convert.ToChar(Data.Count).ToString() + Convert.ToChar(Cant_BFrec).ToString();
+            byte[] Resul = new byte[2+(Cant_BFrec+1)*Data.Count];
+            int posicion = 0;
+            Resul[posicion++] = Convert.ToByte(Convert.ToChar(Data.Count));
+            Resul[posicion++] = Convert.ToByte(Convert.ToChar(Cant_BFrec));
+            int cantidaD = 0;
             foreach (Registro Item in Data)
             {
-                Resul += Item.simbolo;
+                cantidaD += Item.Cant_Aparicion;
+                Resul[posicion++] = Item.simbolo;
                 Aux = Item.Cant_Aparicion % 256;
-                Resul += Convert.ToChar(Aux);
+                Resul[posicion++] = Convert.ToByte(Convert.ToChar(Aux));
                 Aux = Item.Cant_Aparicion - Aux;
                 for (int i=1; i< Cant_BFrec; i++)
                 {
                     if (Aux >= (256 * i)) 
                     {
-                        Resul += Convert.ToChar(256);
+                        Resul[posicion++] = Convert.ToByte(Convert.ToChar(255));
                     }
                     else 
                     {
-                        Resul += Convert.ToChar(" ");
+                        Resul[posicion++] = Convert.ToByte(Convert.ToChar(" "));
                     }
                 }    
             }
             return Resul;
         }
-        public List<Registro> CrearRegistros(string Texto)
+        public void CrearRegistros(byte[] Texto)
         {
-            List<Registro> Data = new List<Registro>();
+
             Cola = new Heap<Registro>();
             Registro Nuevo;
-            int Total= Texto.Length;
-            while(!Texto.Equals(""))
+            int Total = Texto.Length;
+            for (int i = 0; i < Total; i++)
             {
-                string Carct = Convert.ToString(Texto[0]);
                 Nuevo = new Registro();
-                Nuevo.simbolo = Carct;
-                for(int j=0; j < Texto.Length; j++)
+                Nuevo.simbolo = Texto[i];
+                if (Tabla.ContainsKey(Nuevo.simbolo))
                 {
-                    if(Nuevo.simbolo.Equals(Convert.ToString(Texto[j])))
-                    {
-                        Nuevo.Cant_Aparicion++;
-                    }
+                    Tabla[Nuevo.simbolo].Cant_Aparicion++;
                 }
-                Texto = Texto.Replace(Nuevo.simbolo, string.Empty);
-                Nuevo.probabilidad = Convert.ToDouble(Nuevo.Cant_Aparicion) / Convert.ToDouble(Total);
-                Data.Add(Nuevo);
-                Cola.Agregar(Nuevo, Registro.Determinar_Prioridad);
+                else
+                {
+                    Tabla.Add(Texto[i], Nuevo);
+                    Tabla[Nuevo.simbolo].Cant_Aparicion++;
+                }
             }
-            return Data;
+            var Contenido = Tabla.Values;
+            foreach (Registro Item in Contenido)
+            {
+                Item.probabilidad = Convert.ToDouble(Item.Cant_Aparicion) / Convert.ToDouble(Total);
+                Cola.Agregar(Item, Registro.Determinar_Prioridad);
+            }
         }
 
         public void Crear_Arbol()
@@ -116,7 +118,7 @@ namespace ClassLibrary_LAB_03_ED2_URL
             while (Cola.Cant_Nodos > 1)
             {
                 Registro Aux = new Registro();
-                Aux.simbolo = "Nodo" + Cantidad_nodo++;
+                Aux.simbolo = 0;
                 Registro One = Cola.Eliminar(Registro.Comparar_Prioridad, Registro.Determinar_Prioridad, Registro.Comparar_Simbolo);
                 Registro Two = Cola.Eliminar(Registro.Comparar_Prioridad, Registro.Determinar_Prioridad, Registro.Comparar_Simbolo);
                 Aux.probabilidad = One.probabilidad + Two.probabilidad;
@@ -124,10 +126,9 @@ namespace ClassLibrary_LAB_03_ED2_URL
                 Aux.Hijo_Izq = Two;
                 Cola.Agregar(Aux, Registro.Determinar_Prioridad);
             }
-            Agregar_Prefijos(Cola.raiz.Valor);
         }
 
-        public void Agregar_Prefijos(Registro raiz)
+        private void Agregar_Prefijos(Registro raiz)
         {
             try
             {
@@ -140,8 +141,11 @@ namespace ClassLibrary_LAB_03_ED2_URL
                         raiz.Asig_Prefijo("1");
                     if (!string.IsNullOrEmpty(raiz.prefijo))
                     {
-                        if (Compres) { Tabla.Add(raiz.simbolo, raiz); }
-                        else { Tabla.Add(raiz.prefijo, raiz); }
+                        if (Compres) {
+                            Tabla[raiz.simbolo].Asig_Prefijo("1");
+
+                        }
+                        else { Tabla_Decompres.Add(raiz.prefijo, raiz); }
                     }
                     if (raiz.Hijo_Der != null)
                     {
@@ -164,8 +168,12 @@ namespace ClassLibrary_LAB_03_ED2_URL
             Registro_Nodo.Asig_Prefijo(Prefijo_Binario);
             if(!string.IsNullOrEmpty(Registro_Nodo.prefijo))
             {
-                if(Compres) { Tabla.Add(Registro_Nodo.simbolo, Registro_Nodo); }
-                else { Tabla.Add(Registro_Nodo.prefijo, Registro_Nodo); }
+                if(Compres) { 
+                             Tabla[Registro_Nodo.simbolo].Asig_Prefijo(Prefijo_Binario);
+                }
+                else {
+                    Tabla_Decompres.Add(Registro_Nodo.prefijo, Registro_Nodo);
+                }
             }
             if (Registro_Nodo.Hijo_Der != null)
             {
@@ -173,20 +181,19 @@ namespace ClassLibrary_LAB_03_ED2_URL
             }
         }
 
-        private string Binario_Convert(string Cadena)
+        private string Binario_Convert(byte[] Cadena)
         {
             string Binario = "";
             for(int i=0; i<Cadena.Length;i++)
             {
                 Registro aux = new Registro();
-                Binario += Tabla[Convert.ToString(Cadena[i])].prefijo;
+                Binario += Tabla[Cadena[i]].prefijo;
             }
             return Binario;
         }
 
-        private string Convert_Ascii(string Text_Binario)
+        private byte[] Convert_Ascii(string Text_Binario)
         {
-            string Comprimido = "";
             int Cantidad_Byte = Convert.ToInt32(Math.Ceiling(Convert.ToDouble(Text_Binario.Length) / 8));
             string[] Byte = new string[Cantidad_Byte];
             int posicion = 0;
@@ -212,66 +219,71 @@ namespace ClassLibrary_LAB_03_ED2_URL
                 posicion++;
             }
             char[] Caracteres_Resul = new char[Cantidad_Byte];
-            for(int i=0;i<Cantidad_Byte;i++)
+            byte[] Comprimido = new byte[Cantidad_Byte];
+            for (int i=0;i<Cantidad_Byte;i++)
             {
+                //Duda
                 Caracteres_Resul[i] = Convert.ToChar(Convert.ToInt32(Byte[i], 2));
-                Comprimido += Caracteres_Resul[i].ToString(); 
+                Comprimido[i] = Convert.ToByte(Caracteres_Resul[i]); 
             }
             return Comprimido;
         }
     
     
-        public string Descompresion (string Text)
+        public byte[] Descompresion (byte[] Text)
         {
-            Tabla = new Dictionary<string, Registro>();
+            Tabla_Decompres = new Dictionary<string, Registro>();
             Compres = false;
-            string Txt_Comprimido=Get_Data(Text);
+            byte[] Txt_Comprimido=Get_Data(Text);
             Crear_Arbol();
+            Agregar_Prefijos(Cola.raiz.Valor);
             return Get_OriginalText(Convert_Binario(Txt_Comprimido));
         }
 
-        public string Get_Data(string txt)
+        private byte[] Get_Data(byte[] txt)
         {
             Cola = new Heap<Registro>();
             List<Registro> List_aux = new List<Registro>();
             Registro Nuevo;
             int Total = 0;
-            string datos = txt;
+            byte[] datos = txt;
             int Cant_Caract = Convert.ToInt32(Convert.ToChar(datos[0]));
             int Tam_BFrec   = Convert.ToInt32(datos[1]);
-            datos = datos.Remove(0, 2);
+            int posicion= 2;
             for (int i=0;i<Cant_Caract;i++)
             {
                 Nuevo = new Registro();
-                Nuevo.simbolo = Convert.ToString(datos[0]);
-                Nuevo.Cant_Aparicion = Convert.ToInt32(datos[1]);
+                Nuevo.simbolo = datos[posicion++];
+                Nuevo.Cant_Aparicion = Convert.ToInt32(datos[posicion++]);
                 for(int j=2;j<=Tam_BFrec;j++)
                 {
-                    if(!datos[j].Equals(Convert.ToChar(" ")))
+                    if(datos[posicion]!=32)
                     { 
-                    Nuevo.Cant_Aparicion += Convert.ToInt32(datos[j]);
+                    Nuevo.Cant_Aparicion += 256;
                     }
+                    posicion++;
                 }
                 List_aux.Add(Nuevo);
                 Total += Nuevo.Cant_Aparicion;
-                datos = datos.Remove(0, 1+Tam_BFrec);
             }
             foreach (Registro Item in List_aux)
             {
-                Item.probabilidad =  Convert.ToDouble(Item.Cant_Aparicion) / Convert.ToDouble(Total);
+                Item.probabilidad = Convert.ToDouble(Item.Cant_Aparicion) / Convert.ToDouble(Total);
                 Cola.Agregar(Item, Registro.Determinar_Prioridad);
             }
             Tam_Original = Total;
-            return datos;
+            byte[] Data_retorna = new byte[datos.Length-posicion];
+            Array.Copy(datos, posicion, Data_retorna, 0, Data_retorna.Length);
+            return Data_retorna;
         }
 
 
-        private string Convert_Binario(string txt)
+        private string Convert_Binario(byte[] txt)
         {
-            string texto = txt;
+            byte[] texto = txt;
             string txt_binario = "";
             string caract_binario = "";
-            foreach (char Caract in texto)
+            foreach (byte Caract in texto)
             {
                 caract_binario = Convert.ToString(Convert.ToInt32(Caract), 2);
                 while (caract_binario.Length < 8)
@@ -283,21 +295,21 @@ namespace ClassLibrary_LAB_03_ED2_URL
             return txt_binario;
         }
 
-        private string Get_OriginalText(string text)
+        private byte[] Get_OriginalText(string text)
         {
             string data_binaria = text;
-            string Resultado = "";
+            byte[] Resultado = new byte[Tam_Original];
             string aux = "";
             int pos = 0;
             for(int i=0;i<Tam_Original;i++)
             {
                 aux = Convert.ToString(data_binaria[pos]);
-                while (!Tabla.ContainsKey(aux))
+                while (!Tabla_Decompres.ContainsKey(aux))
                 {
                     pos++;
                     aux += Convert.ToString(data_binaria[pos]);
                 }
-                Resultado += Tabla[aux].simbolo;
+                Resultado[i] = Tabla_Decompres[aux].simbolo;
                 pos++;
             }
             return Resultado;
--- ConsoleApp-LAB-03-ED2-URL/Program.cs
+++ ConsoleApp-LAB-03-ED2-URL/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using ClassLibrary_LAB_03_ED2_URL;
 
 namespace ConsoleApp_LAB_03_ED2_URL
@@ -9,24 +10,86 @@ namespace ConsoleApp_LAB_03_ED2_URL
         {
             Huffman CompresorCrack = new Huffman();
             Console.WriteLine("Hello");
-            string paso ="";
-            while (!paso.Equals("si"))
+            //string paso = "";
+            //while (!paso.Equals("si"))
+            //{
+            //    Console.WriteLine("Ingrese el texto");
+            //    string Text = Console.ReadLine();
+            //    byte[] texto = new byte[Text.Length];
+            //    for (int i = 0; i < Text.Length; i++)
+            //    {
+            //        texto[i] = Convert.ToByte(Convert.ToChar(Text[i]));
+            //    }
+            //    byte[] Comprimido = CompresorCrack.Compresion(texto);
+            //    string result = "";
+            //    foreach (byte bit in Comprimido)
+            //    {
+            //        result += Convert.ToString(Convert.ToChar(bit));
+            //    }
+            //    Console.WriteLine(Comprimido.ToString());
+            //    Console.WriteLine(result);
+            //    Console.WriteLine("------------------------------------------------------------------------");
+            //    Console.ReadKey();
+            //    Console.WriteLine("------------------------------------------------------------------------");
+            //    Console.WriteLine("------------------------------------------------------------------------");
+            //    Console.WriteLine(CompresorCrack.Resultado_Obtenido());
+            //    Console.WriteLine("------------------------------------------------------------------------");
+            //    Console.WriteLine("------------------------------------------------------------------------");
+            //    Console.WriteLine("------------------------------------------------------------------------");
+            //    result = "";
+            //    byte[] Descomprimido = CompresorCrack.Descompresion(Comprimido);
+            //    foreach (byte bit in Descomprimido)
+            //    {
+            //        result += Convert.ToString(Convert.ToChar(bit));
+            //    }
+            //    Console.WriteLine(result);
+            //    Console.WriteLine("DESEA PARAR");
+            //    paso = Console.ReadLine();
+            //}
+
+            //using FileStream file = new FileStream("C:\\Users\\Diego Veliz\\Desktop\\cuento.txt", FileMode.OpenOrCreate);
+            //using BinaryReader Lector = new BinaryReader(file);
+            //int Cant_Byte_Read = 10000;
+            //byte[] Text = new byte[Cant_Byte_Read];
+            //Text = Lector.ReadBytes(Cant_Byte_Read);
+            //while (file.Position < file.Length)
+            //{
+            //    byte[] Aux = Lector.ReadBytes(Cant_Byte_Read);
+            //    Array.Resize(ref Text, Text.Length + Aux.Length);
+            //    Aux.CopyTo(Text, Cant_Byte_Read);
+            //}
+            //Lector.Close();
+            //byte[] Impresor = CompresorCrack.Compresion(Text);
+
+            //using FileStream StreFight = new FileStream("C:\\Users\\Diego Veliz\\Desktop\\CuentoComprimido.txt", FileMode.OpenOrCreate);
+            //using BinaryWriter Escritor = new BinaryWriter(StreFight);
+            //Escritor.Write(Impresor);
+            //Escritor.Close();
+
+            //Console.WriteLine(CompresorCrack.Resultado_Obtenido());
+            
+
+
+
+            using FileStream file = new FileStream("C:\\Users\\Diego Veliz\\Desktop\\CuentoComprimido.txt", FileMode.OpenOrCreate);
+            using BinaryReader Lector = new BinaryReader(file);
+            int Cant_Byte_Read = 10000;
+            byte[] Text = new byte[Cant_Byte_Read];
+            Text = Lector.ReadBytes(Cant_Byte_Read);
+            while (file.Position < file.Length)
             {
-                Console.WriteLine("Ingrese el texto");
-                string Text = Console.ReadLine();
-                string Comprimido = CompresorCrack.Compresion(Text);
-                Console.WriteLine(Comprimido);
-                Console.WriteLine("------------------------------------------------------------------------");
-                Console.WriteLine("------------------------------------------------------------------------");
-                Console.WriteLine("------------------------------------------------------------------------");
-                Console.WriteLine(CompresorCrack.Resultado_Obtenido());
-                Console.WriteLine("------------------------------------------------------------------------");
-                Console.WriteLine("------------------------------------------------------------------------");
-                Console.WriteLine("------------------------------------------------------------------------");
-                Console.WriteLine(CompresorCrack.Descompresion(Comprimido));
-                Console.WriteLine("DESEA PARAR");
-                paso = Console.ReadLine();
+                byte[] Aux = Lector.ReadBytes(Cant_Byte_Read);
+                Array.Resize(ref Text, Text.Length + Aux.Length);
+                Aux.CopyTo(Text, Cant_Byte_Read);
             }
+            Lector.Close();
+            byte[] Impresor = CompresorCrack.Descompresion(Text);
+            using FileStream StreFight = new FileStream("C:\\Users\\Diego Veliz\\Desktop\\ResultadoDescomprimido.txt", FileMode.OpenOrCreate);
+            using BinaryWriter Escritor = new BinaryWriter(StreFight);
+            Escritor.Write(Impresor);
+            Escritor.Close();
+
+            Console.WriteLine("Hecho");
             Console.ReadKey();
         }
     }
